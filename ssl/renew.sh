a6fa4c9a6001372e328d959700f834c1c99fed75
Lucas Basquerotto <lucasbasquerotto@example.com>
2019-07-12 19:21:26 -0300

    new structure

--- ssl/renew.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-set -e
-parent_path=$(cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P)
-docker run -it -v /docker/vol/ssl:/etc/ssl -v "$parent_path":/etc/input httpd:2.4.35 openssl x509 -in /etc/ssl/server.csr -out /etc/ssl/server.crt -req -signkey /etc/ssl/server.key -days 36500 -sha256 -extfile /etc/input/v3.ext

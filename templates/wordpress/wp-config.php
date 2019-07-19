67482e9ac66cbd5f5eb2ff75cf0cd38be2ec092c
Lucas Basquerotto <lucasbasquerotto@example.com>
2019-07-18 22:12:54 -0300

    templates

--- templates/wordpress/wp-config.php
+++ templates/wordpress/wp-config.php
@@ -9,12 +9,12 @@
 define('DB_HOST', getenv('DB_HOST'));
 define('DB_NAME', getenv('DB_NAME'));
 define('DB_USER', getenv('DB_USER'));
-define('DB_PASSWORD', '{{ tpl.main.db_password }}');
-define('DB_CHARSET', '{{ params.db_charset | default("utf8mb4") }}');
-define('DB_COLLATE', '{{ params.db_collate | default("") }}');
+define('DB_PASSWORD', '{{ params.db_password }}');
+define('DB_CHARSET', '{{ params.db_charset }}');
+define('DB_COLLATE', '{{ params.db_collate }}');
 
-define('WP_HOME', '{{ tpl.main.protocol }}://{{ tpl.main.domain }}');
-define('WP_SITEURL', '{{ tpl.main.protocol }}://{{ tpl.main.domain }}');
+define('WP_HOME', '{{ params.home }}');
+define('WP_SITEURL', '{{ params.siteurl }}');
 
 // Generate keys automatically at:
 // https://api.wordpress.org/secret-key/1.1/salt/
@@ -27,10 +27,10 @@ define('SECURE_AUTH_SALT', '{{ params.secure_auth_salt }}');
 define('LOGGED_IN_SALT',   '{{ params.logged_in_salt }}');
 define('NONCE_SALT',       '{{ params.nonce_salt }}');
 
-$table_prefix  = '{{ params.table_prefix | default("wp_") }}';
+$table_prefix  = '{{ params.table_prefix }}';
 
-define('WPLANG', '{{ params.wplang | default("en_US") }}');
-define('WP_DEBUG', {{ params.wp_debug | default(false) | lower }});
+define('WPLANG', '{{ params.wplang }}');
+define('WP_DEBUG', '{{ params.wp_debug }}');
 
 // define('DO_NOT_UPGRADE_GLOBAL_TABLES', {{ params.do_not_upgrade_global_tables | default(true) | lower }});
 // define('DISALLOW_FILE_EDIT', {{ params.disallow_file_edit | default(true) | lower }});

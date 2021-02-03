b389171cf3af7b9dfb7c46ee74530014d4e41e08
Lucas Basquerotto <lucasbasquerotto@gmail.com>
2021-02-03 19:40:17 -0300

    [dev] vars

--- shared/scripts/vars/mediawiki.sh
+++ shared/scripts/vars/mediawiki.sh
@@ -10,26 +10,8 @@ errors=()
 
 errors+=("start ...")
 
-export var_custom__pod_type="${var_load__pod_type:-}"
-export var_custom__local="${var_load__local:-}"
-export var_custom__use_main_network="${var_load__use_main_network:-}"
-export var_custom__use_logrotator="${var_load__use_logrotator:-}"
-export var_custom__use_nginx="${var_load__use_nginx:-}"
-export var_custom__use_mysql="${var_load__use_mysql:-}"
-export var_custom__use_fluentd="${var_load__use_fluentd:-}"
-export var_custom__use_theia="${var_load__use_theia:-}"
-export var_custom__use_varnish="${var_load__use_varnish:-}"
-export var_custom__use_custom_ssl="${var_load__use_custom_ssl:-}"
-export var_custom__use_certbot="${var_load__use_certbot:-}"
-
 if [ -z "${var_load__data_dir:-}" ]; then
 	errors+=("var_load__data_dir is not defined")
-fi
-
-if [ "${var_load__local:-}" = 'true' ]; then
-	export var_data_dir_rel="${var_load__data_dir:-}"
-else
-	export var_data_dir="${var_load__data_dir:-}"
 fi
 
 if [ -z "${var_load__pod_type:-}" ]; then
@@ -44,13 +26,43 @@ if [ -z "${var_load__migrate__db_user:-}" ]; then
 	errors+=("var_load__migrate__db_user is not defined")
 fi
 
+tmp_is_web=''
+
+if [ "${var_load__pod_type:-}" = 'app' ] || [ "${var_load__pod_type:-}" = 'web' ]; then
+	tmp_is_web='true'
+fi
+
+tmp_is_db=''
+
+if [ "${var_load__pod_type:-}" = 'app' ] || [ "${var_load__pod_type:-}" = 'db' ]; then
+	tmp_is_db='true'
+fi
+
+if [ "${var_load__local:-}" = 'true' ]; then
+	export var_data_dir_rel="${var_load__data_dir:-}"
+else
+	export var_data_dir="${var_load__data_dir:-}"
+fi
+
+export var_custom__pod_type="${var_load__pod_type:-}"
+export var_custom__local="${var_load__local:-}"
+export var_custom__use_main_network="${var_load__use_main_network:-}"
+export var_custom__use_logrotator="${var_load__use_logrotator:-}"
+export var_custom__use_nginx="${var_load__use_nginx:-}"
+export var_custom__use_mysql="${var_load__use_mysql:-}"
+export var_custom__use_fluentd="${var_load__use_fluentd:-}"
+export var_custom__use_theia="${var_load__use_theia:-}"
+export var_custom__use_varnish="${var_load__use_varnish:-}"
+export var_custom__use_custom_ssl="${var_load__use_custom_ssl:-}"
+export var_custom__use_certbot="${var_load__use_certbot:-}"
+
 export var_run__migrate__db_service='mysql'
 export var_run__migrate__db_name="${var_load__migrate__db_name:-}"
 export var_run__migrate__db_user="${var_load__migrate__db_user:-}"
 export var_run__migrate__db_pass="${var_load__migrate__db_pass:-}"
 export var_run__migrate__db_connect_wait_secs="${var_load__migrate__db_connect_wait_secs:-300}"
 
-if [ "${var_load__pod_type:-}" = 'app' ] || [ "${var_load__pod_type:-}" = 'web' ]; then
+if [ "$tmp_is_web" = 'true' ]; then
 	if [ "${var_load__enable_backup_replica:-}" = 'true' ]; then
 		if [ -z "${var_load__s3_backup__bucket_name:-}" ]; then
 			errors+=("var_load__s3_backup__bucket_name is not defined (s3_replicate_backup)")
@@ -74,13 +86,13 @@ fi
 
 tmp_group_backup=""
 
-if [ "${var_load__pod_type:-}" = 'app' ] || [ "${var_load__pod_type:-}" = 'db' ]; then
+if [ "$tmp_is_db" = 'true' ]; then
 	if [ "${var_load__enable_db_backup:-}" = 'true' ]; then
 		tmp_group_backup="$tmp_group_backup,db_backup"
 	fi
 fi
 
-if [ "${var_load__pod_type:-}" = 'app' ] || [ "${var_load__pod_type:-}" = 'web' ]; then
+if [ "$tmp_is_web" = 'true' ] &&  [ "${var_load__use_s3_storage:-}" != 'true' ]; then
 	if [ "${var_load__enable_uploads_backup:-}" = 'true' ]; then
 		tmp_group_backup="$tmp_group_backup,uploads_backup"
 	fi
@@ -108,13 +120,13 @@ if [ "${var_load__enable_sync_setup:-}" = 'true' ]; then
 	tmp_group_setup="$tmp_group_setup,sync_setup"
 fi
 
-if [ "${var_load__pod_type:-}" = 'app' ] || [ "${var_load__pod_type:-}" = 'web' ]; then
-	if [ "${var_load__enable_uploads_setup:-}" = 'true' ]; then
+if [ "$tmp_is_web" = 'true' ]; then
+	if [ "${var_load__enable_uploads_setup:-}" = 'true' ] &&  [ "${var_load__use_s3_storage:-}" != 'true' ]; then
 		tmp_group_setup="$tmp_group_setup,uploads_setup"
 	fi
 fi
 
-if [ "${var_load__pod_type:-}" = 'app' ] || [ "${var_load__pod_type:-}" = 'db' ]; then
+if [ "$tmp_is_db" = 'true' ]; then
 	if [ "${var_load__enable_db_setup:-}" = 'true' ]; then
 		tmp_group_setup="$tmp_group_setup,db_setup"
 	fi
@@ -134,7 +146,7 @@ export var_task__group_setup__group_task__task_names="$tmp_group_setup"
 
 # Tasks
 
-if [ "${var_load__pod_type:-}" = 'app' ] || [ "${var_load__pod_type:-}" = 'web' ]; then
+if [ "$tmp_is_web" = 'true' ]; then
 	if [ "${var_load__block_ips:-}" = 'true' ]; then
 		export var_shared__block_ips__action_exec__max_amount="${var_load__block_ips__max_amount:-10000}"
 		export var_shared__block_ips__action_exec__amount_day="${var_load__block_ips__amount_day:-20000}"
@@ -145,7 +157,7 @@ if [ "${var_load__pod_type:-}" = 'app' ] || [ "${var_load__pod_type:-}" = 'web' 
 	fi
 fi
 
-if [ "${var_load__pod_type:-}" = 'app' ] || [ "${var_load__pod_type:-}" = 'web' ]; then
+if [ "$tmp_is_web" = 'true' ]; then
 	if [ "${var_load__use_certbot:-}" = 'true' ]; then
 		if [ -z "${var_load__certbot__main_domain:-}" ]; then
 			errors+=("var_load__certbot__main_domain is not defined")
@@ -176,7 +188,7 @@ if [ "${var_load__pod_type:-}" = 'app' ] || [ "${var_load__pod_type:-}" = 'web' 
 	fi
 fi
 
-if [ "${var_load__pod_type:-}" = 'app' ] || [ "${var_load__pod_type:-}" = 'db' ]; then
+if [ "$tmp_is_db" = 'true' ]; then
 	if [ "${var_load__enable_db_backup:-}" = 'true' ]; then
 		tmp_db_src_dir='/tmp/main/tmp/mysql/backup'
 		tmp_db_tmp_dir='/tmp/main/tmp/backup/mysql'
@@ -240,7 +252,7 @@ export var_task__db_main__db_subtask__db_user="${var_load__db_main__db_user:-}"
 export var_task__db_main__db_subtask__db_pass="${var_load__db_main__db_pass:-}"
 export var_task__db_main__db_subtask__db_connect_wait_secs="${var_load__db_main__db_connect_wait_secs:-300}"
 
-if [ "${var_load__pod_type:-}" = 'app' ] || [ "${var_load__pod_type:-}" = 'db' ]; then
+if [ "$tmp_is_db" = 'true' ]; then
 	if [ "${var_load__enable_db_setup:-}" = 'true' ]; then
 		tmp_db_dest_dir='/tmp/main/tmp/mysql/restore'
 		tmp_db_tmp_dir='/tmp/main/tmp/restore/mysql/'
@@ -397,7 +409,7 @@ if [ -n "${var_load__s3_backup__bucket_name:-}" ]; then
 	export var_task__s3_backup__s3_subtask__endpoint="${var_load__s3_backup__endpoint:-}"
 fi
 
-if [ "${var_load__pod_type:-}" = 'app' ] || [ "${var_load__pod_type:-}" = 'web' ]; then
+if [ "$tmp_is_web" = 'true' ]; then
 	if [ "${var_load__enable_backup_replica:-}" = 'true' ]; then
 		export var_task__s3_backup_replica__s3_subtask__alias='backup_replica'
 		export var_task__s3_backup_replica__s3_subtask__service='s3_cli'
@@ -410,7 +422,7 @@ if [ "${var_load__pod_type:-}" = 'app' ] || [ "${var_load__pod_type:-}" = 'web' 
 	fi
 fi
 
-if [ "${var_load__pod_type:-}" = 'app' ] || [ "${var_load__pod_type:-}" = 'web' ]; then
+if [ "$tmp_is_web" = 'true' ]; then
 	if [ -n "${var_load__s3_uploads__bucket_name:-}" ]; then
 		export var_task__s3_uploads__s3_subtask__alias='uploads'
 		export var_task__s3_uploads__s3_subtask__service='s3_cli'
@@ -423,7 +435,7 @@ if [ "${var_load__pod_type:-}" = 'app' ] || [ "${var_load__pod_type:-}" = 'web' 
 	fi
 fi
 
-if [ "${var_load__pod_type:-}" = 'app' ] || [ "${var_load__pod_type:-}" = 'web' ]; then
+if [ "$tmp_is_web" = 'true' ]; then
 	if [ "${var_load__enable_uploads_replica:-}" = 'true' ]; then
 		export var_task__s3_uploads_replica__s3_subtask__alias='uploads_replica'
 		export var_task__s3_uploads_replica__s3_subtask__service='s3_cli'
@@ -472,6 +484,141 @@ if [ "${var_load__enable_sync_setup:-}" = 'true' ]; then
     export var_task__sync_setup__setup_remote__subtask_cmd_s3='s3:subtask:s3_backup'
     export var_task__sync_setup__setup_remote__restore_dest_dir="$tmp_dest_dir"
     export var_task__sync_setup__setup_remote__restore_bucket_path_dir="${var_load__sync_setup__restore_bucket_path_dir:-}"
+fi
+
+if [ "$tmp_is_web" = 'true' ] &&  [ "${var_load__use_s3_storage:-}" != 'true' ]; then
+	if [ "${var_load__enable_uploads_backup:-}" = 'true' ]; then
+		tmp_uploads_src_dir='/var/main/data/mediawiki/uploads'
+		tmp_uploads_tmp_dir='/tmp/main/tmp/backup/uploads'
+
+		tmp_compress_type="${var_load__uploads_backup__compress_type:-zip}"
+		tmp_default_compressed_file_name='uploads.[[ datetime ]].[[ random ]].zip'
+		tmp_compressed_file_name="${var_load__uploads_backup__compressed_file_name:-$tmp_default_compressed_file_name}"
+		tmp_uploads_compress_dest_file="${tmp_uploads_tmp_dir}/${tmp_compressed_file_name}"
+
+		tmp_default_sync_dir='[[ date ]]'
+		tmp_sync_dir="${var_load__uploads_backup__backup_bucket_sync_dir:-$tmp_default_sync_dir}"
+
+		export var_task__uploads_backup__task__type='backup'
+		export var_task__uploads_backup__backup_task__backup_src="$tmp_uploads_src_dir"
+		export var_task__uploads_backup__backup_task__subtask_cmd_remote='backup:remote:default'
+		export var_task__uploads_backup__backup_task__is_compressed_file="${var_load__uploads_backup__is_compressed_file:-}"
+
+		if [ "${var_load__uploads_backup__is_compressed_file:-}" = 'true' ]; then
+			export var_task__uploads_backup__backup_task__compress_type="$tmp_compress_type"
+			export var_task__uploads_backup__backup_task__compress_dest_file="$tmp_uploads_compress_dest_file"
+			export var_task__uploads_backup__backup_task__compress_pass="${var_load__uploads_backup__compress_pass:-}"
+		fi
+
+		export var_task__uploads_backup__backup_task__backup_date_format="${var_load__uploads_backup__backup_date_format:-}"
+		export var_task__uploads_backup__backup_task__backup_time_format="${var_load__uploads_backup__backup_time_format:-}"
+		export var_task__uploads_backup__backup_task__backup_datetime_format="${var_load__uploads_backup__backup_datetime_format:-}"
+		export var_task__uploads_backup__backup_task__recursive_dir="$tmp_uploads_tmp_dir"
+		export var_task__uploads_backup__backup_task__recursive_mode="${var_load__uploads_backup__recursive_mode:-}"
+		export var_task__uploads_backup__backup_task__recursive_mode_dir="${var_load__uploads_backup__recursive_mode_dir:-}"
+		export var_task__uploads_backup__backup_task__recursive_mode_file="${var_load__uploads_backup__recursive_mode_file:-}"
+		export var_task__uploads_backup__backup_task__file_to_clear="${var_load__uploads_backup__file_to_clear:-}"
+		export var_task__uploads_backup__backup_task__dir_to_clear="${var_load__uploads_backup__dir_to_clear:-}"
+
+		if [ -z "${var_load__s3_uploads__bucket_name:-}" ]; then
+			errors+=("var_load__s3_uploads__bucket_name is not defined (uploads_backup)")
+		fi
+
+		export var_task__uploads_backup__backup_remote__subtask_cmd_s3='s3:subtask:s3_uploads'
+		export var_task__uploads_backup__backup_remote__backup_bucket_sync_dir="$tmp_sync_dir"
+		export var_task__uploads_backup__backup_remote__backup_date_format="${var_load__uploads_backup__backup_date_format:-}"
+		export var_task__uploads_backup__backup_remote__backup_time_format="${var_load__uploads_backup__backup_time_format:-}"
+		export var_task__uploads_backup__backup_remote__backup_datetime_format="${var_load__uploads_backup__backup_datetime_format:-}"
+	fi
+
+	if [ "${var_load__enable_uploads_setup:-}" = 'true' ]; then
+		tmp_restore_use_s3="${var_load__uploads_setup__restore_use_s3:-}"
+		tmp_restore_s3_sync="${var_load__uploads_setup__restore_s3_sync:-}"
+		tmp_is_compressed_file="${var_load__uploads_setup__is_compressed_file:-}"
+		tmp_uploads_s3_sync=''
+
+		if [ "$tmp_restore_use_s3" = 'true' ] && [ "$tmp_restore_s3_sync" = 'true' ]; then
+			tmp_uploads_s3_sync='true'
+		fi
+
+		tmp_uploads_dest_dir='/var/main/data/mediawiki/uploads'
+		tmp_uploads_tmp_base_dir='/tmp/main/tmp/mediawiki'
+
+		tmp_default_file_to_skip='/tmp/main/setup/uploads.skip'
+		tmp_file_to_skip="${var_load__uploads_setup__verify_file_to_skip:-$tmp_default_file_to_skip}"
+		tmp_compress_type="${var_load__uploads_setup__compress_type:-zip}"
+
+		tmp_default_compressed_file_name='uploads.zip'
+		tmp_compressed_file_name="${var_load__uploads_setup__compressed_file_name:-$tmp_default_compressed_file_name}"
+		tmp_compressed_file_path="/tmp/main/tmp/restore/uploads/$tmp_compressed_file_name"
+
+		export var_task__uploads_setup__task__type='setup'
+		export var_task__uploads_setup__setup_task__subtask_cmd_remote='setup:remote:default'
+		export var_task__uploads_setup__setup_task__verify_file_to_skip="$tmp_file_to_skip"
+		export var_task__uploads_setup__setup_task__is_compressed_file="${tmp_is_compressed_file:-}"
+
+		if [ "${tmp_is_compressed_file:-}" = 'true' ]; then
+			export var_task__uploads_setup__setup_task__compress_type="$tmp_compress_type"
+			export var_task__uploads_setup__setup_task__compress_src_file="$tmp_compressed_file_path"
+			export var_task__uploads_setup__setup_task__compress_dest_dir="$tmp_uploads_tmp_base_dir"
+			export var_task__uploads_setup__setup_task__compress_pass="${var_load__uploads_setup__compress_pass:-}"
+		fi
+
+		export var_task__uploads_setup__setup_task__recursive_mode="${var_load__uploads_setup__recursive_mode:-}"
+		export var_task__uploads_setup__setup_task__recursive_mode_dir="${var_load__uploads_setup__recursive_mode_dir:-}"
+		export var_task__uploads_setup__setup_task__recursive_mode_file="${var_load__uploads_setup__recursive_mode_file:-}"
+
+		if [ "$tmp_uploads_s3_sync" = 'true' ] || [ "${tmp_is_compressed_file:-}" = 'true' ]; then
+			tmp_compressed_inner_dir="${var_load__uploads_setup__restore_compressed_inner_dir:-}"
+			tmp_uploads_tmp_dir="${tmp_uploads_tmp_base_dir}"
+
+			if [ -n "$tmp_compressed_inner_dir" ]; then
+				tmp_uploads_tmp_dir="${tmp_uploads_tmp_base_dir}/${tmp_compressed_inner_dir}"
+			fi
+
+			export var_task__uploads_setup__setup_task__recursive_dir="${var_load__uploads_setup__recursive_dir:-}"
+			export var_task__uploads_setup__setup_task__move_src="${var_load__uploads_setup__move_src:-}"
+			export var_task__uploads_setup__setup_task__move_dest="${var_load__uploads_setup__move_dest:-}"
+			export var_task__uploads_setup__setup_task__file_to_clear="${var_load__uploads_setup__file_to_clear:-}"
+			export var_task__uploads_setup__setup_task__dir_to_clear="${var_load__uploads_setup__dir_to_clear:-}"
+		else
+			export var_task__uploads_setup__setup_task__recursive_dir="$tmp_uploads_dest_dir"
+		fi
+
+		export var_task__uploads_setup__setup_verify__setup_dest_dir_to_verify="$tmp_uploads_dest_dir"
+		export var_task__uploads_setup__setup_remote__restore_use_s3="$tmp_restore_use_s3"
+
+		if [ "${tmp_restore_use_s3:-}" != 'true' ]; then
+			if [ -z "${var_load__uploads_setup__restore_remote_file:-}" ]; then
+				errors+=("var_load__uploads_setup__restore_remote_file is not defined")
+			fi
+
+			export var_task__uploads_setup__setup_remote__restore_remote_file="${var_load__uploads_setup__restore_remote_file:-}"
+
+			if [ "$tmp_is_compressed_file" = 'true' ]; then
+				export var_task__uploads_setup__setup_remote__restore_dest_file="$tmp_compressed_file_path"
+			else
+				errors+=("uploads_setup: use_s3=false and is_compressed_file=false (at least 1 should be true)")
+			fi
+		else
+			if [ -z "${var_load__s3_uploads__bucket_name:-}" ]; then
+				errors+=("var_load__s3_uploads__bucket_name is not defined (uploads_setup)")
+			fi
+
+			export var_task__uploads_setup__setup_remote__subtask_cmd_s3='s3:subtask:s3_uploads'
+			export var_task__uploads_setup__setup_remote__restore_s3_sync="$tmp_restore_s3_sync"
+			export var_task__uploads_setup__setup_remote__restore_bucket_path_dir="${var_load__uploads_setup__restore_bucket_path_dir:-}"
+			export var_task__uploads_setup__setup_remote__restore_bucket_path_file="${var_load__uploads_setup__restore_bucket_path_file:-}"
+
+			if [ "$tmp_is_compressed_file" = 'true' ]; then
+				export var_task__uploads_setup__setup_remote__restore_dest_dir=''
+				export var_task__uploads_setup__setup_remote__restore_dest_file="$tmp_compressed_file_path"
+			else
+				export var_task__uploads_setup__setup_remote__restore_dest_dir="$tmp_uploads_dest_dir"
+				export var_task__uploads_setup__setup_remote__restore_dest_file=''
+			fi
+		fi
+	fi
 fi
 
 errors+=("... end")

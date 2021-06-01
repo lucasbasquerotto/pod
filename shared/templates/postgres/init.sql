8e1068d37dda23f86b40d11ac17419cbdafb39b1
Lucas Basquerotto <lucasbasquerotto@gmail.com>
2021-05-31 23:46:11 -0300

    [dev] postgres

--- shared/templates/postgres/init.sql
+++ shared/templates/postgres/init.sql
@@ -1,5 +1,47 @@
-CREATE USER IF NOT EXISTS 'viewer'@'%' IDENTIFIED BY '{{ params.viewer_password }}';
-GRANT SELECT, SHOW VIEW, PROCESS, REPLICATION CLIENT, CREATE TEMPORARY TABLES ON *.* TO 'viewer'@'%';
+-- Create the database
+CREATE EXTENSION IF NOT EXISTS dblink;
 
-CREATE USER IF NOT EXISTS '{{ params.db_user }}'@'%' IDENTIFIED BY '{{ params.db_password }}';
-GRANT ALL PRIVILEGES ON `{{ params.db_name }}`.* TO '{{ params.db_user }}'@'%';
+DO $$
+BEGIN
+    PERFORM dblink_exec('', 'CREATE DATABASE {{ params.db_name }}');
+EXCEPTION
+    WHEN duplicate_database THEN
+        RAISE NOTICE '%, skipping', SQLERRM USING ERRCODE = SQLSTATE;
+END
+$$;
+
+-- Create Read-Only user
+DO $$
+BEGIN
+    CREATE ROLE viewer WITH LOGIN PASSWORD '{{ params.viewer_password }}' NOSUPERUSER INHERIT NOCREATEDB NOCREATEROLE NOREPLICATION VALID UNTIL 'infinity';
+EXCEPTION
+    WHEN duplicate_object THEN
+        RAISE NOTICE 'not creating role viewer -- it already exists';
+END$$;
+
+-- Assign permission to this read only user
+GRANT CONNECT ON DATABASE {{ params.db_name }} TO viewer;
+GRANT USAGE ON SCHEMA public TO viewer;
+GRANT SELECT ON ALL TABLES IN SCHEMA public TO viewer;
+GRANT SELECT ON ALL SEQUENCES IN SCHEMA public TO viewer;
+
+-- Assign permissions to read all newly tables created in the future
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT ON TABLES TO viewer;
+
+-- {{ params.db_user }} user
+DO $$
+BEGIN
+    CREATE ROLE {{ params.db_user }} WITH LOGIN PASSWORD '{{ params.db_password }}';
+EXCEPTION
+    WHEN duplicate_object THEN
+        RAISE NOTICE 'not creating role {{ params.db_user }} -- it already exists';
+END$$;
+
+-- Assign permission to the {{ params.db_user }} user
+GRANT CONNECT ON DATABASE {{ params.db_name }} TO {{ params.db_user }};
+GRANT USAGE ON SCHEMA public TO {{ params.db_user }};
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO {{ params.db_user }};
+GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO {{ params.db_user }};
+
+-- Assign permissions to read all newly tables created in the future
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL PRIVILEGES ON TABLES TO {{ params.db_user }};
